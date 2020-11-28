7841f574d3c317087d796092acbc176546bb2f14
unknown <therupak16@gmail.com>
2020-11-28 19:49:46 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+
+ <!-- Font Awesome CSS -->
+ <link rel="stylesheet" href="css/all.min.css">
+
+ <!-- Google Font -->
+ <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+
+ <!-- Custom CSS -->
+ <link rel="stylesheet" href="css/custom.css">
+
+ <title>Rupak's Official Website </title>
+</head>
+<body>
+ <!-- Start Navigation -->
+ <nav class="navbar navbar-expand-sm navbar-dark bg-danger pl-5 fixed-top">
+  <a href="index.php" class="navbar-brand">Rupak's Offical Website</a>
+  <span class="navbar-text"></span>
+  <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+   <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="myMenu">
+   <ul class="navbar-nav pl-5 custom-nav">
+     <li class="nav-item"><a href="index.php" class="nav-link"> <i class="fas fa-home"></i>Home</a></li>
+   <!--  <li class="nav-item"><a href="#Services" class="nav-link"> <i class="fas fa-hand-paper"> </i>Services</a></li>
+    <li class="nav-item"><a href="#registration" class="nav-link"> <i class="fas fa-clipboard"></i> Registration</a></li>
+    <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link"> <i class="fas fa-location-arrow"></i>Login</a></li> -->
+    <li class="nav-item"><a href="#Contact" class="nav-link"> <i class="fas fa-compass"></i>Contact</a></li>
+
+   </ul>
+  </div>
+ </nav> <!-- End Navigation -->
+
+ <!-- Start Header Jumbotron-->
+ <header class="jumbotron back-image" style="background-image:url(images/rupak.jpg);">
+  <div class="myclass mainHeading">
+   <h4 class="text-uppercase text-danger font-weight-bold">Welcome to my official website</h4>
+   <p class="font-italic">Here you can find my projects I've done and recently completed</p>
+   <!-- <a href="Requester/RequesterLogin.php" class="btn btn-success mr-4">Login</a>
+   <a href="#registration" class="btn btn-danger mr-4">Sign Up</a> -->
+  </div>
+ </header> <!-- End Header Jumbotron-->
+
+ <!-- Start Introduction Section -->
+ <div class="container">
+  <div class="jumbotron">
+   <h3 class="text-center">About me</h3>
+   <p>
+    I am a full-time student pursuing an undergraduate degree in Mechanical Engineering at the University of Texas at Arlington. I am enthuastic in researching and solving problems in the field of dynamics/Robotics
+    <h4 class="text-center"> Education Attainment </h4>
+    <ul>
+    <p> <li> UNIVERSITY OF TEXAS AT ARLINGTON </li> </p>
+    <P> <li>  EXPECTED GRADUATION : MAY 21 </li> </P>
+    <P> <li> B.S MECHANICAL ENGINEERING </li>  </P>
+    <p> <li>       GPA 3.735/4.00 </li> </p>
+  </ul>
+    
+   </p>   
+   </p>
+   <h4 class="text-center"> RELEVANT COURSES </h4>
+   <ul>
+    <p> <li> Machine Design </li> </p>
+    <P> <li> Kinematics and Dynamics </li> </P>
+    <P> <li> Dyanmic Systems Mod & Sim </li>  </P>
+    <p> <li> Robotics </li> </p>
+    <p> <li> Computational Aerodynamcs </li> </p>
+    <p> <li> Thermodynamics and Heat Transfer </li> </p>
+    <p> <li> Manufacturing </li> </p>
+    <p> <li> Mechanics of Materials </li> </p>
+    <p> <li> Measurement Methods </li> </p>
+  </ul>
+  <h4 class="text-center"> SKILLS </h4>
+  <h5 class="text-left"> Computer Skills </h5>
+   <ul>
+    <p> <li> MATLAB/Simulink </li> </p>
+    <P> <li> Solidworks </li> </P>
+    <P> <li> C/C++ </li>  </P>
+    <p> <li> ANSYS </li> </p>
+    <p> <li> MS-OFFICE </li> </p>
+    <p> <li> Autolev</li> </p>
+  </ul>
+  <h5 class="text-left">Technical Skills </h5>
+  <ul>
+    <p> <li> Engineering and Problem Solving </li> </p>
+    <p> <li> Technical Reporting </li> </p>
+    <p> <li> System Modeling </li> </p>
+  </ul>
+
+
+  </div>
+ </div> 
+ <div class="container">
+  <div class="jumbotron">
+   <h3 class="text-center">Research and Projects </h3>
+   <ul>
+     <li> Surgical Drill End-Affector -Desiging the end-effector for the drill for cutting station robot which resembles the compliant property of a
+human hand which will be used mostly for fatigue testing.
+ </li>
+<li> Five bar linkage robot powered by Arduino -
+Robot desgined in solid-works, 3d printed and programmed using Arduino to draw 2-D shapes. Implementation
+of Operational space control and EOM developed using Kane's method in MATLAB and Autolev
+ </li>
+ <li>Desiging Gravity car-
+Team Leader in project which involved designing and building a prototype using SolidWorks and 3D-printing </li>
+<li>MATLAB simulation for dynamics of 6DOF object.
+Forward dynamics simulation that will solve the EOM and perform numerical integration on the calculated
+accelerations using quaternions instead of Euler angles to remove singularity problem.
+  </li>
+  <li>Kinematics analysis of a 3 DOF system (containing motor, gear, linkage and pulley) in MATLAB-
+Forward dynamics simulation calculating speed, acceleration and torque for associated free bodies </li>
+<li> 2-D heat transfer analysis of microprocessor chip-
+Heat transfer equation for the system determined using central difference method and surface plot generated in
+MATLAB showing temperature variation for given boundary condition. </li>
+<li>Operational space control and Joint space control simulation of two link manipulator with gears using Autolev and
+MATLAB </li>
+<li>
+ANSYS simulation of bicycle pedal system to determine safety of factor, fatigue testing and stress analysis</li>
+
+
+   </ul>
+  </div>
+</div>
+<div class="container">
+  <div class="jumbotron">
+   <h3 class="text-center">WORK EXPERIENCE </h3>
+   <ol>
+   <h4 class="text-left">Supplemental instruction Leader/Tutor </h4>
+<li>SI leader for MAE 1351 course (Introduction to Engineering design). (Spring 2019-Fall 2019) </li>
+<li>SI Leader for ENGR 1250 (Engineering Problem solving) Fall 2019 </li>
+</ol>
+ </div>
+</div>
+<div class="container">
+  <div class="jumbotron">
+   <h3 class="text-center">AWARDS</h3>
+   <ul>
+     <li> Maverick Academic Scholarship, MAE Marie & Bill Sampson Scholarship. </li>
+   </ul>
+ </div>
+</div>
+ 
+ <!-- Start Contact US -->
+ <div class="jumbotron bg-secondary">
+ <div class="container" id="Contact">
+  <h2 class="text-center mb-4">Contact me</h2>
+  <div class="row">
+   <!-- Start 1st Column -->
+    <?php include('contactform.php') ?>
+   <!-- End 1st Column -->
+   <div class="col-md-4 text-center"> <!-- Start 2nd Column -->
+    <strong>Rupak Luitel:</strong><br>
+   <br>
+    Texas,USA<br>
+  
+    Mobile:(682)414-8306 <br>
+    <a href="#" target="_blank">www.rupakluitel.com.np</a><br>
+    <br> <br>
+       
+   </div> <!-- End 2nd Column -->
+  </div>
+ </div> <!-- End Contact US -->
+
+<!-- Start Footer -->
+ <footer class="container-fluid bg-dark mt-5 text-white">
+  <div class="container">
+   <div class="row py-3">
+    <div class="col-md-8"> <!-- Start 1st Column -->
+     <span class="pr-4">Find me on : </span>
+
+
+     <a href="https://www.facebook.com/theRupak18/" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+     <a href="https://twitter.com/" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i></a>
+     <a href="https://youtube.com/" target="_blank" class="pr-2 fi-color"><i class="fab fa-youtube"></i></a>
+        <!--  <li class="nav-item"><a href="Admin/login.php"class="nav-link"> <i class="fas fa-user-secret"></i>Admin login</a></li>
+     -->
+    </div> <!-- End 1st Column -->
+    <div class="col-md-7 text-right"> <!-- Start 2nd Column -->
+
+    </div> <!-- End 2nd Column -->
+   </div>
+  </div>
+  
+ </footer>
+
+ <!-- JavaScript -->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/popper.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/all.min.js"></script>
+</body>
+</html>
+
+ <!-- JavaScript -->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/popper.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/all.min.js"></script>
+</body>
+</html>
